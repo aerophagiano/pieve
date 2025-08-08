4eccc86d2fecbc393bc5180ce6ddb4dd42bec962
Federico <federico.destefanis@gmail.com>
2025-08-08 15:49:36 +0200

    fix autoclose hamburger nav

---
+++ close-nav.js
@@ -0,0 +1,14 @@
+  document.addEventListener("DOMContentLoaded", function () {
+	debugger;
+    var navLinks = document.querySelectorAll('.navbar-collapse .nav-link');
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+
+    navLinks.forEach(function (link) {
+      link.addEventListener('click', function () {
+        // Only collapse if navbar is currently shown
+        if (navbarCollapse.classList.contains('show')) {
+          new bootstrap.Collapse(navbarCollapse).toggle();
+        }
+      });
+    });
+  });
